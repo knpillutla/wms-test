e2f554b0dd0bce913c68651633a8d40661d18909
Krishna Pillutla <knpillutla@gmail.com>
2018-09-29 18:52:01 -0400

    Adding Content

--- src/test/java/com/test/EventReceiver.java
+++ src/test/java/com/test/EventReceiver.java
@@ -2,22 +2,28 @@ package com.test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 
-import com.example.inventory.dto.events.InventoryCreatedEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class EventReceiver {
 	Consumer consumer;
 	String topicName;
 	ObjectMapper mapper = new ObjectMapper();
+
 	public EventReceiver(String consumerGroup, String topicName) {
 		this.topicName = topicName;
 		Properties props = new Properties();
@@ -30,33 +36,96 @@ public class EventReceiver {
 //		 props.put("ssl.endpoint.identification.algorithm", "HTTPS");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("spring.cloud.stream.kafka.bindings.inventory-out.consumer.autoCommitOffset", true);
+		props.put("spring.cloud.stream.kafka.bindings." + topicName + ".consumer.autoCommitOffset", false);
+		//props.put("spring.cloud.stream.kafka.bindings." + topicName + "inventory-out.consumer.enable.auto.commit", false);
+		props.put("enable.auto.commit", false);
+		props.put("auto.commit.interval.ms", "1000");
 		props.put("group.id", consumerGroup);
-		props.put("enable.auto.commit",true);
 		consumer = new KafkaConsumer<>(props);
+		//Collection<TopicPartition> topicPartitionList = null;
 		consumer.subscribe(Arrays.asList(topicName));
-		ConsumerRecords<String, String> records = consumer.poll(10000);
+		// consumer.poll(0);
+		// consumer.commitSync();
+/*		consumer.subscribe(Arrays.asList(topicName), new ConsumerRebalanceListener() {
+			Map<TopicPartition, Long> offsetMap = new HashMap();
+			@Override
+			public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+				// do nothing
+				long position = 0 ;
+				System.out.println("Revoked started:" + consumer);
+				Map<TopicPartition, Long> offsets = consumer.endOffsets(partitions);
+				for (Entry<TopicPartition, Long> entry : offsets.entrySet()) {
+					position = consumer.position(entry.getKey());
+					offsetMap.put(entry.getKey(), position);
+					System.out.println("revoked, consumer position:" + position);
+				}
+				System.out.println("Revoked ended:" + consumer);
+			}
+			
+
+			@Override
+			public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+				Map<TopicPartition, Long> offsets = consumer.endOffsets(partitions);
+				for (Entry<TopicPartition, Long> entry : offsets.entrySet()) {
+					// this only gets executed the first time, we do poll. So, on a creation of the
+					// consumer, we need to do a poll to make sure.
+					long position = consumer.position(entry.getKey());
+					Long lastRevokedPosition = offsetMap.get(entry.getKey());
+					System.out.println("Assigning offset, current position:" + position + ", lastRevokedPosition:" + lastRevokedPosition);
+					if(lastRevokedPosition != null) 
+					{
+						position = lastRevokedPosition;
+						System.out.println("Seeting current position to last revoked position:" + position);
+					}
+					consumer.seek(entry.getKey(), position);
+					System.out
+					.println("consumer, seeking position to :" + entry.getKey() + ": position:" + position);
+
+				}
+			}
+		});*/
+		
+		
+/*		Map<TopicPartition, Long> offsets = consumer.endOffsets(topicPartitionList);
+		for (Entry<TopicPartition, Long> entry : offsets.entrySet()) {
+			// this only gets executed the first time, we do poll. So, on a creation of the
+			// consumer, we need to do a poll to make sure.
+			System.out.println(entry.getKey() + "," + entry.getValue());
+			consumer.seek(entry.getKey(), entry.getValue());
+			System.out
+					.println("consumer:" + entry.getKey() + ": position:" + consumer.position(entry.getKey()));
+			consumer.seek(entry.getKey(), consumer.position(entry.getKey()));
+			System.out
+			.println("consumer, seeking position to :" + entry.getKey() + ": position:" + (consumer.position(entry.getKey())));
+
+		}*/
+		consumer.poll(100);
 		consumer.commitSync();
 	}
-	
+
 	public <T> List<T> getEvent(Class<T> cls) throws Exception {
 		List<T> eventList = new ArrayList();
-		ConsumerRecords<String, String> records = consumer.poll(10000);
-		consumer.commitSync();
-		int recordCount = records.count();
-		System.out.println("count of records:" + recordCount);
-		ConsumerRecord record = null;
-		int i = 0;
-		Iterator itr = records.iterator();
-		while (itr.hasNext()) {
-			i++;
-			record = (ConsumerRecord) itr.next();
-			System.out.println("Event Receiver, record.value:" + record.value().toString());
-			T obj = (T) mapper.readValue(record.value().toString(), cls);
-			eventList.add(obj);
-			System.out.println("Event Receiver, received event:" + obj);
-			System.out.println("loop:" + i + ",record count:" + recordCount);
+		ConsumerRecords<String, String> records;
+		while ((records = consumer.poll(2000)).count() > 0) {
+			consumer.commitSync();
+			int recordCount = records.count();
+			System.out.println("count of records:" + recordCount);
+			ConsumerRecord record = null;
+			int i = 0;
+			Iterator itr = records.iterator();
+			while (itr.hasNext()) {
+				i++;
+				record = (ConsumerRecord) itr.next();
+				System.out.println("Event Receiver, record.value:" + record.value().toString());
+				T obj = (T) mapper.readValue(record.value().toString(), cls);
+				eventList.add(obj);
+				System.out.println("Event Receiver, received event:" + obj);
+				System.out.println("loop:" + i + ",record count:" + recordCount);
+			}
 		}
-		System.out.println("count of records:" + recordCount);
+		consumer.commitSync();
+		//consumer.commitSync();
+		System.out.println("count of records:" + eventList.size());
 		return eventList;
-	}}
+	}
+}
--- src/test/java/com/test/WareshouseCustomerOrderInventoryCreationTest.java
+++ src/test/java/com/test/WareshouseCustomerOrderInventoryCreationTest.java
@@ -16,16 +16,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.inventory.dto.events.ASNUPCReceivedEvent;
+import com.example.inventory.dto.events.InventoryAllocatedEvent;
 import com.example.inventory.dto.events.InventoryCreatedEvent;
 import com.example.inventory.dto.requests.InventoryCreationRequestDTO;
 import com.example.order.dto.events.CustomerOrderCreatedEvent;
 import com.example.order.dto.events.CustomerOrderDownloadEvent;
 import com.example.order.dto.events.OrderCreatedEvent;
 import com.example.order.dto.events.OrderPackedEvent;
+import com.example.order.dto.events.OrderPickedEvent;
 import com.example.order.dto.events.OrderPlannedEvent;
 import com.example.order.dto.requests.CustomerOrderCreationRequestDTO;
 import com.example.order.dto.requests.OrderFulfillmentRequestDTO;
 import com.example.order.dto.responses.OrderFulfillmentResponseDTO;
+import com.example.packing.dto.events.PackConfirmationEvent;
 import com.example.packing.dto.events.PackCreatedEvent;
 import com.example.packing.dto.requests.PackConfirmRequestDTO;
 import com.example.packing.dto.responses.PackDTO;
@@ -34,7 +37,6 @@ import com.example.picking.dto.events.PickConfirmationEvent;
 import com.example.picking.dto.events.PickCreatedEvent;
 import com.example.picking.dto.requests.PickConfirmRequestDTO;
 import com.example.picking.dto.responses.PickDTO;
-import com.example.shipping.dto.events.ShipCreatedEvent;
 import com.example.shipping.dto.events.ShipRoutingCompletedEvent;
 import com.example.test.service.EventPublisher;
 
@@ -66,7 +68,7 @@ public class WareshouseCustomerOrderInventoryCreationTest {
 	String pickingPort = "9013";
 	String packingPort = "9014";
 	String shippingPort = "9015";
-	
+
 	@Test
 	public void createInventoryAndCustomerOrdersOneOrdeLinePerCustomerOrder() throws Exception {
 		String busName = "XYZ";
@@ -75,7 +77,7 @@ public class WareshouseCustomerOrderInventoryCreationTest {
 		String company = "IE";
 		String division = "09";
 		String userId = "Krishna";
-		int numOfOrders = 1;
+		int numOfOrders = 20;
 		int numOfOrderLines = 1; // num of order lines
 		EventReceiver custOrderEventReceiver = new EventReceiver("whse-customerOrder-consumer",
 				wmsStreams.CUSTOMER_ORDERS_OUTPUT);
@@ -93,56 +95,79 @@ public class WareshouseCustomerOrderInventoryCreationTest {
 					inventoryReq.getLocnNbr(), inventoryReq.getBusUnit(), inventoryReq.getItemBrcd(),
 					inventoryReq.getQty());
 			EventPublisher.send(wmsStreams.inboundInventory(), upcReceivedEvent, upcReceivedEvent.getHeaderMap());
-			List<InventoryCreatedEvent> inventoryEventList = inventoryEventReceiver
-					.getEvent(InventoryCreatedEvent.class);
-			invnCreatedEventList.addAll(inventoryEventList);
 		}
+		invnCreatedEventList.addAll(inventoryEventReceiver.getEvent(InventoryCreatedEvent.class));
 		System.out.println("Inventory Created....");
 
-		Assert.assertEquals(numOfOrders*numOfOrderLines, invnCreatedEventList.size());
+		Assert.assertEquals(numOfOrders * numOfOrderLines, invnCreatedEventList.size());
 		List<CustomerOrderCreationRequestDTO> orderCreationReqList = CustomerOrderCreator
 				.createNewCustomerOrders(invnCreatedEventList, numOfOrders, numOfOrderLines);
 		for (CustomerOrderCreationRequestDTO orderCreationReq : orderCreationReqList) {
 			CustomerOrderDownloadEvent orderDloadEvent = new CustomerOrderDownloadEvent(orderCreationReq);
 			EventPublisher.send(wmsStreams.inboundCustomerOrders(), orderDloadEvent, orderDloadEvent.getHeaderMap());
-			List<CustomerOrderCreatedEvent> tmpCustomerOrderEventList = custOrderEventReceiver
-					.getEvent(CustomerOrderCreatedEvent.class);
-			customerOrderCreatedEventList.addAll(tmpCustomerOrderEventList);
 		}
-		Assert.assertEquals(1, customerOrderCreatedEventList.size());
+		customerOrderCreatedEventList.addAll(custOrderEventReceiver.getEvent(CustomerOrderCreatedEvent.class));
+		Assert.assertEquals(numOfOrders, customerOrderCreatedEventList.size());
 		System.out.println("CustomerOrders Created....");
 		List<OrderCreatedEvent> orderEventList = orderEventReceiver.getEvent(OrderCreatedEvent.class);
-		Assert.assertEquals(1, orderEventList.size());
-		
+		Assert.assertEquals(numOfOrders, orderEventList.size());
+
+
 		// plan order fulfillment
-		String batchNbr = invokeOrderFulfillmentForWarehouse(busName, locnNbr, company, division, busUnit,
-				RandomStringUtils.random(6, true, false), 5);
+		int MaxNumOfOrdersInBatch = 5;
+		List<String> batchNbrList = invokeOrderFulfillmentForWarehouse(busName, locnNbr, company, division, busUnit,
+				RandomStringUtils.random(6, true, false), numOfOrders, MaxNumOfOrdersInBatch, orderEventList);
+
+		// this will be two events, order planned and order allocated.somehow, both orderplanned and order allocated are treated same as the json structure is same
+		List<OrderPlannedEvent> orderPlannedEventList = orderEventReceiver.getEvent(OrderPlannedEvent.class);
+		Assert.assertEquals(numOfOrders*2, orderPlannedEventList.size());
+
+		List<InventoryAllocatedEvent> invnAllocatedEventList = inventoryEventReceiver.getEvent(InventoryAllocatedEvent.class);
+		System.out.println("Inventory Allocated....");
+		Assert.assertEquals(numOfOrders * numOfOrderLines, invnAllocatedEventList.size());
+
 		// ensure picks are created
 		List<PickCreatedEvent> pickCreatedEventList = pickEventReceiver.getEvent(PickCreatedEvent.class);
-		Assert.assertEquals(1, pickCreatedEventList.size());
+		Assert.assertEquals(numOfOrders * numOfOrderLines, pickCreatedEventList.size());
 
+		// check order allocated events
+/*		List<OrderPlannedEvent> orderAllocatedEventList = orderEventReceiver.getEvent(OrderAllocatedEvent.class);
+		Assert.assertEquals(numOfOrders*2, orderAllocatedEventList.size());
+*/
 		// ensure ship is created
-		List<ShipCreatedEvent> shipEventList = shipEventReceiver.getEvent(ShipCreatedEvent.class);
-		Assert.assertEquals(1, shipEventList.size());
-
+		/*
+		 * List<ShipCreatedEvent> shipEventList =
+		 * shipEventReceiver.getEvent(ShipCreatedEvent.class);
+		 * Assert.assertEquals(numOfOrders, shipEventList.size());
+		 */
 		// ensure ship is routed
-		List<ShipRoutingCompletedEvent> shipRoutingCompletedEventList = shipEventReceiver.getEvent(ShipRoutingCompletedEvent.class);
-		Assert.assertEquals(1, shipRoutingCompletedEventList.size());
+		List<ShipRoutingCompletedEvent> shipRoutingCompletedEventList = shipEventReceiver
+				.getEvent(ShipRoutingCompletedEvent.class);
+		Assert.assertEquals(numOfOrders, shipRoutingCompletedEventList.size());
 
-		String containerNbr = this.pickAllOrders(busName, locnNbr, company, division, busUnit, numOfOrders,
-				numOfOrderLines, batchNbr, RandomStringUtils.random(6, true, false));
+		List<String> toteList = this.pickAllOrders(busName, locnNbr, company, division, busUnit, numOfOrders,
+				numOfOrderLines, batchNbrList, RandomStringUtils.random(6, true, false));
 		List<PickConfirmationEvent> pickConfirmationEventList = pickEventReceiver.getEvent(PickConfirmationEvent.class);
-		Assert.assertEquals(1, pickConfirmationEventList.size());
+		Assert.assertEquals(numOfOrders * numOfOrderLines, pickConfirmationEventList.size());
+
+		// confirm order picked
+		List<OrderPickedEvent> orderPickedEventList = orderEventReceiver.getEvent(OrderPickedEvent.class);
+		Assert.assertEquals(numOfOrders, orderPickedEventList.size());
+		
 		// ensure packs are created
 		List<PackCreatedEvent> packCreatedEventList = packEventReceiver.getEvent(PackCreatedEvent.class);
-		Assert.assertEquals(1, packCreatedEventList.size());
+		Assert.assertEquals(numOfOrders * numOfOrderLines, packCreatedEventList.size());
 
 		// pack the task
-		packAllOrders(busName, locnNbr, company, division, busUnit, numOfOrders, numOfOrderLines, batchNbr,
-				RandomStringUtils.random(6, true, false), containerNbr);
+		packAllOrders(busName, locnNbr, company, division, busUnit, numOfOrders, numOfOrderLines,
+				RandomStringUtils.random(6, true, false), toteList);
+
+		// ensure packs are confirmed
+		List<PackConfirmationEvent> packConfirmedEventList = packEventReceiver.getEvent(PackConfirmationEvent.class);
+		Assert.assertEquals(numOfOrders * numOfOrderLines, packConfirmedEventList.size());
 
 		List<OrderPackedEvent> orderPackedEventList = orderEventReceiver.getEvent(OrderPackedEvent.class);
-		Assert.assertEquals(1, orderPackedEventList.size());
+		Assert.assertEquals(numOfOrders, orderPackedEventList.size());
 	}
 
 	// @Test
@@ -157,78 +182,113 @@ public class WareshouseCustomerOrderInventoryCreationTest {
 		Assert.assertEquals(1, orderPlannedEventList.size());
 	}
 
-	public String invokeOrderFulfillmentForWarehouse(String busName, Integer locnNbr, String company, String division,
-			String busUnit, String userId, int numOfOrdersInBatch) {
-		OrderFulfillmentRequestDTO orderFulfillmentReq = new OrderFulfillmentRequestDTO();
-		orderFulfillmentReq.setBusName(busName);
-		orderFulfillmentReq.setLocnNbr(locnNbr);
-		orderFulfillmentReq.setCompany(company);
-		orderFulfillmentReq.setDivision(division);
-		orderFulfillmentReq.setBusUnit(busUnit);
-		orderFulfillmentReq.setWarehouseMode(true);
-		orderFulfillmentReq.setNumOfOrders(numOfOrdersInBatch);
+	public List<String> invokeOrderFulfillmentForWarehouse(String busName, Integer locnNbr, String company,
+			String division, String busUnit, String userId, int numOfOrders, int maxNumOfOrdersInBatch,
+			List<OrderCreatedEvent> orderCreatedEventList) {
+		List<String> batchNbrList = new ArrayList();
+
+		int currentOrderCount = 0;
+		List<Long> orderIdList = new ArrayList();
 		String batchNbr = "";
 		RestTemplate restTemplate = new RestTemplate();
-		OrderFulfillmentResponseDTO response = restTemplate.postForObject(
-				"http://localhost:" + orderPlannerPort + "/orders/v1/" + busName + "/" + locnNbr, orderFulfillmentReq,
-				OrderFulfillmentResponseDTO.class);
-		batchNbr = response.getBatchNbr();
-		System.out.println("order fulfillment response for warehouse:" + response);
-		return batchNbr;
+		for (int i = 0; i < orderCreatedEventList.size(); i++) {
+			OrderCreatedEvent orderCreatedEvent = orderCreatedEventList.get(i);
+			orderIdList.add(orderCreatedEvent.getOrderDTO().getId());
+			currentOrderCount++;
+			if (currentOrderCount >= 5 || ((i + 1) == orderCreatedEventList.size())) {
+				currentOrderCount = 0;
+				OrderFulfillmentRequestDTO orderFulfillmentReq = new OrderFulfillmentRequestDTO();
+				orderFulfillmentReq.setBusName(busName);
+				orderFulfillmentReq.setLocnNbr(locnNbr);
+				orderFulfillmentReq.setCompany(company);
+				orderFulfillmentReq.setDivision(division);
+				orderFulfillmentReq.setBusUnit(busUnit);
+				orderFulfillmentReq.setWarehouseMode(true);
+				orderFulfillmentReq.setOrderIdList(orderIdList);
+				OrderFulfillmentResponseDTO response = restTemplate.postForObject(
+						"http://localhost:" + orderPlannerPort + "/orders/v1/" + busName + "/" + locnNbr,
+						orderFulfillmentReq, OrderFulfillmentResponseDTO.class);
+				batchNbr = response.getBatchNbr();
+				System.out.println("batch nbr from start order fulfillment:" + batchNbr);
+				batchNbrList.add(batchNbr);
+				System.out.println("order fulfillment response for warehouse:" + response);
+				orderIdList = new ArrayList();
+			}
+		}
+		return batchNbrList;
 	}
 
-	public String pickAllOrders(String busName, Integer locnNbr, String company, String division, String busUnit,
-			int numOfOrders, int numOfOrderLines, String batchNbr, String userId) {
+	public List<String> pickAllOrders(String busName, Integer locnNbr, String company, String division, String busUnit,
+			int numOfOrders, int numOfOrderLines, List<String> batchNbrList, String userId) {
 		RestTemplate restTemplate = new RestTemplate();
 		int numOfPicks = numOfOrders * numOfOrderLines;
-		String containerNbr = 5 + RandomStringUtils.random(19, false, true);
-		for (int i = 0; i < numOfPicks; i++) {
-			String pickAssignURL = "http://localhost:" + pickingPort + "/picking/v1/" + busName + "/" + locnNbr + "/picks/next/" + batchNbr + "/" + userId;
+		List<String> toteList = new ArrayList();
+		
+		for (int j = 0; j < batchNbrList.size(); j++) {
+			String batchNbr = batchNbrList.get(j);
+			System.out.println("Batch Nbr:" + batchNbr);
+			String containerNbr = 5 + RandomStringUtils.random(19, false, true);
+			String pickAssignURL = "http://localhost:" + pickingPort + "/picking/v1/" + busName + "/" + locnNbr
+					+ "/picks/next/" + batchNbr + "/" + userId;
 			System.out.println("pick assign url:" + pickAssignURL);
-			// assign next pick
 			PickDTO pickDTO = restTemplate.postForObject(pickAssignURL, null, PickDTO.class);
 			System.out.println("next pick:" + pickDTO);
-			// pick confirm the pick
-			PickConfirmRequestDTO pickConfirmObj = new PickConfirmRequestDTO(pickDTO.getId(), pickDTO.getOrderId(),
-					pickDTO.getBatchNbr(), pickDTO.getBusName(), pickDTO.getLocnNbr(), pickDTO.getBusUnit(),
-					pickDTO.getCompany(), pickDTO.getDivision(), pickDTO.getOrderNbr(), pickDTO.getLocnBrcd(),
-					pickDTO.getItemBrcd(), pickDTO.getQty(), containerNbr, userId);
-			String pickConfirmURL = "http://localhost:" + pickingPort + "/picking/v1/" + busName + "/" + locnNbr + "/picks/"
-					+ pickDTO.getId();
-			System.out.println("pick confirm url:" + pickConfirmURL);
-			PickDTO pickConfirmDTO = restTemplate.postForObject(pickConfirmURL, pickConfirmObj, PickDTO.class);
-			System.out.println("Pick Confirm Reponse:" + pickConfirmDTO);
+			toteList.add(containerNbr); // create one tote per container to start with, later we can add tote capacity
+			while (pickDTO != null) {
+				// assign next pick
+				// pick confirm the pick
+				PickConfirmRequestDTO pickConfirmObj = new PickConfirmRequestDTO(pickDTO.getId(), pickDTO.getOrderId(),
+						pickDTO.getBatchNbr(), pickDTO.getBusName(), pickDTO.getLocnNbr(), pickDTO.getBusUnit(),
+						pickDTO.getCompany(), pickDTO.getDivision(), pickDTO.getOrderNbr(), pickDTO.getLocnBrcd(),
+						pickDTO.getItemBrcd(), pickDTO.getQty(), containerNbr, userId);
+				String pickConfirmURL = "http://localhost:" + pickingPort + "/picking/v1/" + busName + "/" + locnNbr
+						+ "/picks/" + pickDTO.getId();
+				System.out.println("pick confirm url:" + pickConfirmURL);
+				PickDTO pickConfirmDTO = restTemplate.postForObject(pickConfirmURL, pickConfirmObj, PickDTO.class);
+				System.out.println("Pick Confirm Reponse:" + pickConfirmDTO);
+				try {
+					pickDTO = restTemplate.postForObject(pickAssignURL, null, PickDTO.class);
+					System.out.println("next pick:" + pickDTO);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					System.out.println("No more picks available");
+					pickDTO = null;
+					break;
+				}
+			}
 		}
-		return containerNbr;
+		return toteList;
 	}
 
 	public void packAllOrders(String busName, Integer locnNbr, String company, String division, String busUnit,
-			int numOfOrders, int numOfOrderLines, String batchNbr, String userId, String containerNbr) {
+			int numOfOrders, int numOfOrderLines, String userId, List<String> toteNbrList) {
 		RestTemplate restTemplate = new RestTemplate();
-		int numOfPicks = numOfOrders * numOfOrderLines;
-		String packsGETURL = "http://localhost:" + packingPort + "/packing/v1/" + busName + "/" + locnNbr + "/packs/container/"
-				+ containerNbr;
-		System.out.println("pack getpacks url:" + packsGETURL);
-		// List<PackDTO> packDTOList = restTemplate.getForObject(packsGETURL,
-		// List.class);
-		ResponseEntity<List<PackDTO>> response = restTemplate.exchange(packsGETURL, HttpMethod.GET, null,
-				new ParameterizedTypeReference<List<PackDTO>>() {
-				});
-		List<PackDTO> packDTOList = response.getBody();
+		for (int j = 0; j < toteNbrList.size(); j++) {
+			String toteNbr = toteNbrList.get(j);
+			String packsGETURL = "http://localhost:" + packingPort + "/packing/v1/" + busName + "/" + locnNbr
+					+ "/packs/container/" + toteNbr;
+			System.out.println("pack getpacks url:" + packsGETURL);
+			// List<PackDTO> packDTOList = restTemplate.getForObject(packsGETURL,
+			// List.class);
+			ResponseEntity<List<PackDTO>> response = restTemplate.exchange(packsGETURL, HttpMethod.GET, null,
+					new ParameterizedTypeReference<List<PackDTO>>() {
+					});
+			List<PackDTO> packDTOList = response.getBody();
 
-		System.out.println("pack getpacks response:" + packDTOList);
-		for (int i = 0; i < packDTOList.size(); i++) {
-			PackDTO packDTO = packDTOList.get(i);
-			// pack confirm
-			PackConfirmRequestDTO packConfirmObj = new PackConfirmRequestDTO(packDTO.getId(), packDTO.getOrderId(),
-					packDTO.getBatchNbr(), packDTO.getBusName(), packDTO.getLocnNbr(), packDTO.getBusUnit(),
-					packDTO.getCompany(), packDTO.getDivision(), packDTO.getOrderNbr(), packDTO.getItemBrcd(),
-					packDTO.getQty(), containerNbr, "", userId);
-			String packConfirmURL = "http://localhost:" + packingPort + "/packing/v1/" + busName + "/" + locnNbr + "/packs/"
-					+ packDTO.getId();
-			System.out.println("pack confirm url:" + packConfirmURL);
-			PackDTO pickConfirmDTO = restTemplate.postForObject(packConfirmURL, packConfirmObj, PackDTO.class);
-			System.out.println("Pack Confirm Reponse:" + pickConfirmDTO);
+			System.out.println("pack getpacks response:" + packDTOList);
+			for (int i = 0; i < packDTOList.size(); i++) {
+				PackDTO packDTO = packDTOList.get(i);
+				// pack confirm
+				PackConfirmRequestDTO packConfirmObj = new PackConfirmRequestDTO(packDTO.getId(), packDTO.getOrderId(),
+						packDTO.getBatchNbr(), packDTO.getBusName(), packDTO.getLocnNbr(), packDTO.getBusUnit(),
+						packDTO.getCompany(), packDTO.getDivision(), packDTO.getOrderNbr(), packDTO.getItemBrcd(),
+						packDTO.getQty(), toteNbr, "", userId);
+				String packConfirmURL = "http://localhost:" + packingPort + "/packing/v1/" + busName + "/" + locnNbr
+						+ "/packs/" + packDTO.getId();
+				System.out.println("pack confirm url:" + packConfirmURL);
+				PackDTO pickConfirmDTO = restTemplate.postForObject(packConfirmURL, packConfirmObj, PackDTO.class);
+				System.out.println("Pack Confirm Reponse:" + pickConfirmDTO);
+			}
 		}
 	}
 
@@ -243,8 +303,10 @@ public class WareshouseCustomerOrderInventoryCreationTest {
 		int numOfOrders = 1;
 		int numOfOrderLines = 1; // num of order lines
 		String batchNbr = "20180927110815";
+		List<String> batchNbrList = new ArrayList();
+		batchNbrList.add(batchNbr);
 
-		this.pickAllOrders(busName, locnNbr, company, division, busUnit, numOfOrders, numOfOrderLines, batchNbr,
+		this.pickAllOrders(busName, locnNbr, company, division, busUnit, numOfOrders, numOfOrderLines, batchNbrList,
 				userId);
 	}
 
